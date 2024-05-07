2f45b8864c295c6a29be23952de7f273faaed4d0
Syuq <tatd.wine@gmail.com>
2024-05-08 03:59:08 +0700

    🌐 Add internationalization supportChanges: Added a new file i18n.ts to handle internationalization. Implemented lazy loading of translations based on the user's language. Defined fallback languages for unsupported locales.

---
+++ web/src/i18n.ts
@@ -0,0 +1,42 @@
+import i18n, { BackendModule, FallbackLng, FallbackLngObjList } from "i18next";
+import { initReactI18next } from "react-i18next";
+import { findNearestMatchedLanguage } from "./utils/i18n";
+
+export const locales = ["en", "vi"] as const;
+
+const fallbacks = {
+  "zh-HK": ["zh-Hant", "en"],
+  "zh-TW": ["zh-Hant", "en"],
+  zh: ["zh-Hans", "en"],
+} as FallbackLngObjList;
+
+const LazyImportPlugin: BackendModule = {
+  type: "backend",
+  init: function () {},
+  read: function (language, _, callback) {
+    const matchedLanguage = findNearestMatchedLanguage(language);
+    import(`./locales/${matchedLanguage}.json`)
+      .then((translation: any) => {
+        callback(null, translation);
+      })
+      .catch(() => {
+        // Fallback to English.
+      });
+  },
+};
+
+i18n
+  .use(LazyImportPlugin)
+  .use(initReactI18next)
+  .init({
+    detection: {
+      order: ["navigator"],
+    },
+    fallbackLng: {
+      ...fallbacks,
+      ...{ default: ["en"] },
+    } as FallbackLng,
+  });
+
+export default i18n;
+export type TLocale = (typeof locales)[number];
