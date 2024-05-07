f0f7359b023e993266baaae9e598f6a65f2ff5d6
Syuq <tatd.wine@gmail.com>
2024-05-08 04:01:33 +0700

    ✏️ style: Update import statements formatting in main.tsx
    Reformat import statements in main.tsx for consistency and readability. Move global CSS imports to the top and organize other imports alphabetically. This improves code maintainability and enhances developer experience.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./css/global.css";
+import "./css/tailwind.css";
+import "./helpers/polyfill";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
