6bbf30e8eb9cdce9f59853acabb2fcc525c4a756
Syuq <tatd.wine@gmail.com>
2024-05-15 14:07:18 +0700

    ♻️ Refactor Empty component format
    - Reformatted the Empty component for better readability.
    - Removed unnecessary import of ClassNames.
    - Ensured consistent indentation and spacing.

--- web/src/components/Empty.tsx
+++ web/src/components/Empty.tsx
@@ -1,12 +1,11 @@
-import { ClassNames } from "@emotion/react";
 import Icon from "./Icon";
 
 const Empty = () => {
-    return (
-        <div className="mx-auto">
-            <Icon.Bird strokeWidth={1} className="w-24 h-auto text-gray-500 dark:text-gray-400" />
-        </div>
-        );
+  return (
+    <div className="mx-auto">
+      <Icon.Bird strokeWidth={1} className="w-24 h-auto text-gray-500 dark:text-gray-400" />
+    </div>
+  );
 };
 
 export default Empty;
