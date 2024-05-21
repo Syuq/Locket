d85dfb8897d7927f5ad82277195255df4f40bb69
Syuq <tatd.wine@gmail.com>
2024-05-22 01:13:57 +0700

    🌱 (Add or update seed files) - Since you are introducing new seed files to populate database tables.

---
+++ store/db/sqlite/seed/10003__locket_organizer.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+  locket_organizer (`locket_id`, `user_id`, `pinned`)
+VALUES
+  (1, 101, 1);
+
+INSERT INTO
+  locket_organizer (`locket_id`, `user_id`, `pinned`)
+VALUES
+  (3, 101, 1);
